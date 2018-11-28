bea53f95c34345c7ee255ff691b88c39269c4f05
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-11-28 23:47:06 +0700

    Import algorithm from princeton

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 class Main {
 
     public static void main(String [] args) {
 
-        //
+        boolean opened = false;
+        String infile = "in/words_5757.txt";
+        Scanner scan = new Scanner(System.in);
+
+        WordGraph graph = null;
+
+        while (!opened) {
+            try (Scanner file = new Scanner(new File(infile))) {
+
+                opened = true;
+                graph = new WordGraph(file);
+
+            } catch (FileNotFoundException e) {
+                System.err.println(infile + " File Not Found.");
+                System.out.print("Enter data file = ");
+                infile = scan.nextLine();
+            }
+        }
+
+        System.out.print("Enter 5-letter word 1 = ");
+        String word1 = scan.nextLine();
+
+        System.out.print("Enter 5-letter word 2 = ");
+        String word2 = scan.nextLine();
+
+        System.out.println();
+
+        graph.wordLadder(word1, word2);
 
     }
 
--- src/WordGraph.java
+++ src/WordGraph.java
@@ -1,11 +1,52 @@
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+import java.util.Scanner;
 
 public class WordGraph {
 
-    private SimpleDirectedWeightedGraph SG;
-    private DijkstraShortestPath<String, Integer> DSP;
+    private IndexSET<String> words = new IndexSET<>();
+    private Graph G;
 
+    public WordGraph(Scanner file) {
 
+        while (file.hasNext()) {
+            String word = file.nextLine();
+            words.add(word);
+        }
+
+        G = new Graph(words.size());
+        for (String word1 : words.keys()) {
+            for (String word2 : words.keys()) {
+                if (word1.length() != word2.length())
+                    throw new RuntimeException("Words have different lengths");
+                if (word1.compareTo(word2) < 0 && isNeighbor(word1, word2)) {
+                    G.addEdge(words.indexOf(word1), words.indexOf(word2));
+                }
+            }
+        }
+
+    }
+
+    public void wordLadder(String from, String to) {
+        if (!words.contains(from)) throw new RuntimeException(from + " is not in word list");
+        if (!words.contains(to))   throw new RuntimeException(to   + " is not in word list");
+
+        BreadthFirstPaths bfs = new BreadthFirstPaths(G, words.indexOf(from));
+        if (bfs.hasPathTo(words.indexOf(to))) {
+            StdOut.println("length = " + bfs.distTo(words.indexOf(to)));
+            for (int v : bfs.pathTo(words.indexOf(to)))
+                StdOut.println(words.keyOf(v));
+        }
+        else StdOut.println("NOT CONNECTED");
+        StdOut.println();
+    }
+
+    private static boolean isNeighbor(String a, String b) {
+        assert a.length() == b.length();
+        int differ = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) differ++;
+            if (differ > 1) return false;
+        }
+        return true;
+    }
 
 }
